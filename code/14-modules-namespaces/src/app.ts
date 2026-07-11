ee62861a5d014e8bbdd06a54bf844425f2b4e5a9
himanshusaini0018 <107019612+himanshusaini0018@users.noreply.github.com>
2026-07-11 20:35:33 +0530

    notes

--- code/14-modules-namespaces/src/app.ts
+++ code/14-modules-namespaces/src/app.ts
@@ -1,5 +1,9 @@
 import { ProjectInput } from './components/project-input.js';
 import { ProjectList } from './components/project-list.js';
+// Special syntax for namespaces
+/// <reference path=""/>
+
+
 
 new ProjectInput();
 new ProjectList('active');
